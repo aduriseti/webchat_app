87e051032f2a0dfad74958db0baafb78d450dc19
Amal <aduriseti@gmail.com>
2016-03-31 15:23:02 -0700

    problems with opening connections

--- bundle.js
+++ bundle.js
@@ -49,67 +49,94 @@
 	//var peer = new Peer({key: 'lwjd5qra8257b9'});
 	//var peer = new Peer({key: 'bdy0b8k85qrrudi'});
 	var peer = new Peer({host: 'localhost', port: 9000});
-	//var connection = null;
+	var connection = null;
 	peer.on('open', function(id) {
 		console.log('My peer ID is: ' + id);
-		document.write('My peer ID is: ' + id);
+		//document.write('My peer ID is: ' + id);
+		var node = document.createElement("LI"); 
+		var textnode = document.createTextNode('My peer ID is: ' + id);         // Create a text node
+		node.appendChild(textnode);                              // Append the text to <li>
+		document.getElementById("body").appendChild(node);     // Append <li> to <ul> with id="myList"
 		var peer_ID = window.prompt("Enter ID of peer to connect to.", "Peer ID");
 		if (peer_ID != null) {
-			var connection = peer.connect(peer_ID);
+			/*connection = peer.connect(peer_ID);
 			if (connection != null) {
 				connection.on('open', function() {
 					//greet new peer
 					console.log("Connected to peer");
-					document.write("Connected to peer");
+					//document.write("Connected to peer");
 					connection.send("Hello peer!");
 				});
 				connection.on('data', function(data) {
-					//reply to messages
-					var message = window.prompt("Recieved: " + data);
-					connection.send(message);
+					receive_message(data);
 				});
-	}
+			}*/
+			var conn = peer.connect(peer_ID);
+			if (conn != null) {
+				conn.on('open', function() {
+					//greet new peer
+					console.log("Connected to peer");
+					//document.write("Connected to peer");
+					conn.send("Hello peer!");
+				});
+				conn.on('data', function(data) {
+					receive_message(data);
+				});
+			}		
 		}
 	});
 
 	peer.on('connection', function(conn) {
 		console.log("recieved connection");
-		var connection = conn;
+		/*onnection = conn;
 		connection.on('open', function() {
 			//greet new peer
 			console.log("Connected to peer");
-			document.write("Connected to peer");
+			//document.write("Connected to peer");
 			connection.send("Hello peer!");
 		});
 		connection.on('data', function(data) {
-			//reply to messages
-			var message = window.prompt("Recieved: " + data);
-			connection.send(message);
+			receive_message(data);
+		});*/
+		conn.on('open', function() {
+			//greet new peer
+			console.log("Connected to peer");
+			//document.write("Connected to peer");
+			conn.send("Hello peer!");
+		});
+		conn.on('data', function(data) {
+			receive_message(data);
 		});
 	});
 
-
-
-
-	/*if (connection != null) {
-		connection.on('open', function() {
-			//greet new peer
-			console.log("Connected to peer");
-			document.write("Connected to peer");
-			connection.send("Hello peer!");
-		});
-		connection.on('data', function(data) {
-			//reply to messages
-			var message = window.prompt("Recieved: " + data);
-			connection.send(message);
-		});
+	function receive_message(msg) {
+		document.getElementById("log_box").value = msg;
 	}
 
-	/*var send_message = function () {
-
+	function send_message(msg) {
+		/*var node = document.createElement("LI"); 
+		var textnode = document.createTextNode('click');         // Create a text node
+		node.appendChild(textnode);                              // Append the text to <li>
+		document.getElementById("body").appendChild(node);     // Append <li> to <ul> with id="myList"
+		*/
+		if (connection != null) {
+			connection.send(msg);
+			document.getElementById("input_box").value = "";
+		} else {
+			document.getElementById("log_box").value = "Connection not established - message not sent";
+		}
 	}
 
-	document.getElementById("send_button").onclick = function*/
+	document.getElementById("send_button").onclick = function () {
+		send_message(document.getElementById("input_box").value);
+	}
+
+	/*document.getElementById("send_button").onclick = function () {
+		var node = document.createElement("LI"); 
+		var textnode = document.createTextNode('click');         // Create a text node
+		node.appendChild(textnode);                              // Append the text to <li>
+		document.getElementById("body").appendChild(node);     // Append <li> to <ul> with id="myList"
+	}*/
 
 /***/ }
 /******/ ]);
